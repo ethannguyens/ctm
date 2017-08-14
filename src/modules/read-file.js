1e5c9bc13028b52aca21142658d67cc8b43d9224
Ethan <ethan.nguyens@gmail.com>
2017-08-14 22:43:56 +0100

    add necessary dependencies

---
+++ src/modules/read-file.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+class ReadFile {
+  constructor(path, type = 'stream', encode = 'utf8') {
+    this.path = path;
+    this.type = type;
+    this.encode = encode;
+
+    this.uniqueWords = {};
+
+    this.countWords = this.countWords.bind(this);
+    this.stream = this.stream.bind(this);
+
+    this.stream();
+  }
+
+  countWords(line) {
+    const words = line.match(/(?!\d)(\w+\b)/g, '');
+
+    let word;
+    for (let i in words) {
+      word = words[i].toLowerCase();
+      this.uniqueWords[word] ? this.uniqueWords[word] += 1 : this.uniqueWords = 1;
+    }
+  }
+
+  stream() {
+    let remaining = '';
+
+    fs.createReadStream(this.path, this.encode)
+      .on('data', function(chunk) {
+        let index = remaining.indexOf('\n');
+        let line;
+
+        remaining += chunk;
+
+        while (index > -1) {
+          line = remaining.substring(0, index);
+          remaining = remaining.substring(index + 1);
+          this.uniqueWords = self.countWords(line);
+          index = remaining.indexOf('\n');
+        }
+      });
+
+    return this.uniqueWords;
+  }
+}
+
+export default ReadFile;
---
+++ src/modules/read-file.test.js
@@ -0,0 +1,5 @@
+import ReadFile from './read-file';
+
+const data = new ReadFile('../test.txt');
+
+console.log(data);
